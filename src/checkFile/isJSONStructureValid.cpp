4fba11a905874f83861358223cb7a0883ce3f1ac
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2025-03-10 10:32:16 +0300

    Update isJSONStructureValid.cpp

--- src/checkFile/isJSONStructureValid.cpp
+++ src/checkFile/isJSONStructureValid.cpp
@@ -8,24 +8,27 @@
 
 
 
-//Вспомогательная функция проверки файла на неповреждённость JSON-структуры
 bool CheckFile::isJSONStructureValid(const std::string &fileName)
 {
+    //Создать объект для проверки файла
     std::ifstream inFile(fileName);
-
+    //Создать временный объект для поверки
     JSON tmpJSON;
 
     try
     {
+        //Прочитать JSON-структуру
         inFile >> tmpJSON;
     }
     catch (const nlohmann::detail::parse_error &e)
     {
         inFile.close();
+        //JSON-структура повреждена
         return false;
     }
 
     inFile.close();
+    //JSON-структура целостна
     return true;
 }
 
