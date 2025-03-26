2f24bd4092a4f50300bc51b5831b0bb70710f363
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2025-03-26 13:15:30 +0300

    Update testCheckFile.cpp

--- tests/testCheckFile.cpp
+++ tests/testCheckFile.cpp
@@ -1,1 +1,37 @@
+#include "gtest/gtest.h"
 
+#include "checkFile.h"
+#include "checkFileException.h"
+#include "general.h"
+
+
+
+void testCheckFile(const std::string& filePath, const JSON& template, ErrorCode errorCode)
+{
+    try 
+    {
+    checkFile(filePath, template);
+    FAIL();
+    }
+    catch (std::exception& e)
+    {
+      ASSERT_STREQ(e.getErrorCode(), errorCode);
+    }
+}
+
+TEST(TestCheckFile, configExist)
+{
+  std::string wrongFilePath{wrongConfig.json};  
+
+  testCheckFile(wrongConfig.json, constants::configTemplate, ERROR_FILE_MISSING);
+}
+
+TEST(TestCheckFile, configJSONStructureValid)
+{
+  testCheckFile(constants::configFilePath, constants::configTemplate, ERROR_FILE_STRUCTURE_CORRUPTED);
+}
+
+TEST(TestCheckFile, configJSONStructureMatch)
+{
+  testCheckFile(constants::configFilePath, constants::configTemplate, ERROR_FILE_STRUCTURE_NOT_MATCH);
+}
