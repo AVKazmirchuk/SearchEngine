c4d9e3fdd95f33be9fdc8abe9fa90d4105e51ff4
AVKazmirchuk <a_kazmirchuk@mail.ru>
2025-08-22 15:38:50 +0300

    refactor

--- kav_check/tests/testGeneral.cpp
+++ kav_check/tests/testGeneral.cpp
@@ -139,7 +139,8 @@ void putFiles()
     std::filesystem::remove("file001.txt");
     std::filesystem::remove("requests.json");
     std::filesystem::remove("answers.json");
-
+    std::filesystem::remove("connectDisk.bat");
+    std::filesystem::remove("disconnectDisk.bat");
 
     std::filesystem::remove_all("resources");
 
@@ -150,6 +151,8 @@ void putFiles()
     std::filesystem::copy("../../tests/resources/file001.txt", "file001.txt");
     std::filesystem::copy("../../tests/resources/requests.json", "requests.json");
     std::filesystem::copy("../../tests/resources/answers.json", "answers.json");
+    std::filesystem::copy("../../tests/resources/connectDisk.bat", "connectDisk.bat");
+    std::filesystem::copy("../../tests/resources/disconnectDisk.bat", "disconnectDisk.bat");
 
     std::filesystem::copy("../../tests/resources/resources", "resources");
 }
