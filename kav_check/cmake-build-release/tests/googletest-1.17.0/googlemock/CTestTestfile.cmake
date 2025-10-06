f103bd846afc8784a0388c740410f1e35a1466a7
AVKazmirchuk <a_kazmirchuk@mail.ru>
2025-10-06 19:20:06 +0300

    refactor

---
+++ kav_check/cmake-build-release/tests/googletest-1.17.0/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Alexander/CLionProjects/search_engine/kav_check/tests/googletest-1.17.0/googlemock
+# Build directory: C:/Users/Alexander/CLionProjects/search_engine/kav_check/cmake-build-release/tests/googletest-1.17.0/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
