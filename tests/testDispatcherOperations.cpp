4a3f5f5eb82d7a85c3c515da2c67ea645d8fa81e
AVKazmirchuk <a_kazmirchuk@mail.ru>
2026-01-12 16:48:23 +0300

    refactor testDispatcherOperations

---
+++ tests/testDispatcherOperations.cpp
@@ -0,0 +1,31 @@
+//
+// Created by AKazmirchuk on 12.01.2026.
+//
+
+
+
+#include "gtest/gtest.h"
+
+#include "dispatcherOperations.h"
+
+
+
+TEST(TestDispatcherOperations_writeJSONFile, general)
+{
+
+}
+
+TEST(TestDispatcherOperations_writeJSONFile, message)
+{
+
+}
+
+TEST(TestDispatcherOperations_writeJSONFile, errorLevel)
+{
+
+}
+
+TEST(TestDispatcherOperations_writeJSONFile, callingFunction)
+{
+
+}
